963641db6701cd7ccc284cb38239ebdf5537df0b
Lea Rosema <lea@lea.lgbt>
2025-02-09 23:50:33 +0100

    chore: add basic events

---
+++ src/textui/ui_event.cpp
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#ifdef __DOS__
+#include <conio.h>
+#else
+#include <stubs.h>
+#endif
+#include "ui_event.h"
+#include "mouse.h"
+
+static MOUSE_STATUS lastMouseStatus;
+
+void poll_event(ui_event_t *result)
+{
+	ui_event_t event = {0};
+
+	MOUSE_STATUS mouseStatus = {0};
+
+	mouse_get_status(&mouseStatus);
+	if ((mouseStatus.buttons & 1 > 0) && (lastMouseStatus.buttons & 1 == 0))
+	{
+		lastMouseStatus.buttons = mouseStatus.buttons;
+		event.type = UI_EVENT_MOUSEDOWN;
+		event.payload.mouse.x = mouseStatus.mouseX;
+		event.payload.mouse.y = mouseStatus.mouseY;
+		event.payload.mouse.buttons = mouseStatus.buttons;
+		*result = event;
+		return;
+	}
+
+	if ((mouseStatus.buttons & 1 > 0) && (lastMouseStatus.buttons & 1 == 0))
+	{
+		lastMouseStatus.buttons = mouseStatus.buttons;
+		event.type = UI_EVENT_MOUSEUP;
+		event.payload.mouse.x = mouseStatus.mouseX;
+		event.payload.mouse.y = mouseStatus.mouseY;
+		event.payload.mouse.buttons = mouseStatus.buttons;
+		*result = event;
+		return;
+	}
+
+	if ((mouseStatus.mouseX != lastMouseStatus.mouseX) || (mouseStatus.mouseY != lastMouseStatus.mouseY)) {
+		lastMouseStatus.mouseX = mouseStatus.mouseX;
+		lastMouseStatus.mouseY = mouseStatus.mouseY;
+		event.type = UI_EVENT_MOUSEMOVE;
+		event.payload.mouse.x = mouseStatus.mouseX;
+		event.payload.mouse.y = mouseStatus.mouseY;
+		event.payload.mouse.buttons = mouseStatus.buttons;
+		return;
+	}
+
+	if ((kbhit())) {
+		event.type = UI_EVENT_KEY;
+		event.payload.keyboard.keyCode = getch();
+		return;
+	}
+}
--- src/textui/ui_event.h
+++ src/textui/ui_event.h
@@ -2,17 +2,21 @@
 #define __UI_EVENT_H__
 
 #include <stdint.h>
+#include "mouse.h"
 
 #define UI_EVENT_KEYDOWN    1
 #define UI_EVENT_KEYUP			2
-#define UI_EVENT_MOUSEDOWN  4
-#define UI_EVENT_MOUSEUP    8
-#define UI_EVENT_CLICK			16
+#define UI_EVENT_KEY				4
+#define UI_EVENT_MOUSEDOWN  8
+#define UI_EVENT_MOUSEUP    16
+#define UI_EVENT_CLICK			32
+#define UI_EVENT_MOUSEMOVE	64
 
 typedef struct ui_mouse_event_s
 {
-	uint16_t mouseX;
-	uint16_t mouseY;
+	uint16_t x;
+	uint16_t y;
+	uint16_t buttons;
 } ui_mouse_event_t;
 
 typedef struct ui_keyboard_event_s
@@ -26,7 +30,7 @@ typedef struct ui_event_s
 	union event_payload {
 		ui_mouse_event_t mouse;
 		ui_keyboard_event_t keyboard;
-	};
+	} payload;
 } ui_event_t;
 
 void poll_event(ui_event_t *event);
