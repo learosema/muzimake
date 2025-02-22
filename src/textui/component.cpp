fe9db1264caf7f0bfb19999e70b3c4c11c212d27
Lea Rosema <lea@lea.lgbt>
2025-02-22 23:48:45 +0100

    chore: more work

--- src/textui/component.cpp
+++ src/textui/component.cpp
@@ -1,8 +1,122 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "component.h"
 #include "textmode.h"
 #include "macros.h"
+
+bool rect_test_point_in_bounds(rect_t * rect, uint8_t x, uint8_t y)
+{
+	uint8_t x0 = rect->x;
+	uint8_t x1 = x0 + rect->width - 1;
+	uint8_t y0 = rect->y;
+	uint8_t y1 = y0 + rect->height - 1;
+	return (
+		x >= x0 && x <= x1 && y >= y0 && y <= y1
+	);
+}
+
+bool rect_test_mouse(rect_t * rect, uint16_t mouseX, uint16_t mouseY)
+{
+	return rect_test_point_in_bounds(rect, (uint8_t)mouseX/8, (uint8_t)mouseY/8);
+}
+
+void button_render(ui_button_t *button)
+{
+	uint8_t color = button->color;
+	if (button->active) {
+		color = color & 0x0f;
+	}
+
+	if (button->focused) {
+		textmode_dblrect(
+			button->bounding_rect.x,
+			button->bounding_rect.y,
+			button->bounding_rect.width,
+			button->bounding_rect.height,
+			color
+		);
+	} else {
+		textmode_rect(
+			button->bounding_rect.x,
+			button->bounding_rect.y,
+			button->bounding_rect.width,
+			button->bounding_rect.height,
+			color
+		);
+	}
+	if (button->bounding_rect.width <= 2 || button->bounding_rect.height <= 2) {
+		return;
+	}
+
+	int len = MIN(strlen(button->label), button->bounding_rect.width-2);
+	uint8_t centerX = (uint8_t)(button->bounding_rect.x + button->bounding_rect.width/2);
+	uint8_t centerY = (uint8_t)(button->bounding_rect.y + button->bounding_rect.height/2);
+	uint8_t labelX = (uint8_t)(centerX - len/2);
+	uint8_t innerX = button->bounding_rect.x + 1;
+	uint8_t innerY = button->bounding_rect.y + 1;
+	uint8_t innerWidth = button->bounding_rect.width - 2;
+	uint8_t innerHeight = button->bounding_rect.height - 2;
+	for (uint8_t j = 0; j < innerHeight; j++) {
+		uint8_t currentY = innerY + j;
+		if (currentY != centerY) {
+			textmode_hline(innerX, currentY, innerWidth, ' ', color);
+		} else {
+			uint8_t leftWidth = labelX - innerX;
+			textmode_hline(innerX, centerY, leftWidth, ' ', color);
+			textmode_hline(labelX + len, centerY, innerWidth - len - leftWidth, ' ', color);
+			textmode_print(button->label, labelX, centerY, color);
+		}
+	}
+}
+
+void button_process_events(ui_button_t *button, ui_event_t *event)
+{
+	if (button->event_handler != nullptr) {
+		bool result = button->event_handler(button->id, event);
+		if (result == false) {
+			return;
+		}
+	}
+	switch (event->type) {
+		case UI_EVENT_MOUSEMOVE:
+			if ((button->active)&&
+					(rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)))
+			{
+				button_render(button);
+			}
+			break;
+		case UI_EVENT_MOUSEDOWN:
+			if (rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)) {
+				button->active = true;
+				button_render(button);
+			}
+			break;
+		case UI_EVENT_MOUSEUP:
+			if (button->active) {
+				button->active = false;
+				button_render(button);
+				if (rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)) {
+					if (button->event_handler != nullptr) {
+						ui_event_t click = {0};
+						click.type = UI_EVENT_CLICK;
+						click.payload.click.buttons = event->payload.mouse.buttons;
+						click.payload.click.target = button->id;
+						bool result = button->event_handler(button->id, &click);
+						if (result == false) {
+							return;
+						}
+					}
+				}
+			}
+			break;
+		case UI_EVENT_KEY:
+			// todo: shortcuts?
+			break;
+		default:
+			break;
+	}
+}
 
 void input_render(ui_input_t *input)
 {
@@ -57,7 +171,7 @@ void input_render(ui_input_t *input)
 	{
 		size_t offs = input->cursor_x0 + i;
 		char ch = offs < value_len ? input->value[offs] : '.';
-		textmode_putchar(ch, input_x, input_y, color);
+		textmode_putchar(ch, input_x + i, input_y, color);
 	}
 
 	if (input->focused) {
@@ -65,115 +179,75 @@ void input_render(ui_input_t *input)
 	}
 }
 
-void button_render(ui_button_t *button)
+void input_process_events(ui_input_t *input, ui_event_t *event)
 {
-	uint8_t color = button->color;
-	if (button->active) {
-		color = color & 0x0f;
-	}
-
-	if (button->focused) {
-		textmode_dblrect(
-			button->bounding_rect.x,
-			button->bounding_rect.y,
-			button->bounding_rect.width,
-			button->bounding_rect.height,
-			color
-		);
-	} else {
-		textmode_rect(
-			button->bounding_rect.x,
-			button->bounding_rect.y,
-			button->bounding_rect.width,
-			button->bounding_rect.height,
-			color
-		);
-	}
-	if (button->bounding_rect.width <= 2 || button->bounding_rect.height <= 2) {
-		return;
-	}
-
-	int len = MIN(strlen(button->label), button->bounding_rect.width-2);
-	uint8_t centerX = (uint8_t)(button->bounding_rect.x + button->bounding_rect.width/2);
-	uint8_t centerY = (uint8_t)(button->bounding_rect.y + button->bounding_rect.height/2);
-	uint8_t labelX = (uint8_t)(centerX - len/2);
-	uint8_t innerX = button->bounding_rect.x + 1;
-	uint8_t innerY = button->bounding_rect.y + 1;
-	uint8_t innerWidth = button->bounding_rect.width - 2;
-	uint8_t innerHeight = button->bounding_rect.height - 2;
-	for (uint8_t j = 0; j < innerHeight; j++) {
-		uint8_t currentY = innerY + j;
-		if (currentY != centerY) {
-			textmode_hline(innerX, currentY, innerWidth, ' ', color);
-		} else {
-			uint8_t leftWidth = labelX - innerX;
-			textmode_hline(innerX, centerY, leftWidth, ' ', color);
-			textmode_hline(labelX + len, centerY, innerWidth - len - leftWidth, ' ', color);
-			textmode_print(button->label, labelX, centerY, color);
+	switch (event->type) {
+		case UI_EVENT_MOUSEUP: {
+			ui_event_t clickEvent = {0};
+			clickEvent.type = UI_EVENT_CLICK;
+			clickEvent.payload.click.target = input->id;
+			clickEvent.payload.click.buttons = event->payload.mouse.buttons;
+			if (! rect_test_mouse(&(input->bounding_rect), event->payload.mouse.x, event->payload.mouse.y))
+			{
+				if (input->focused) {
+					input->focused = false;
+					input_render(input);
+				}
+				break;
+			}
+			if (input->event_handler != nullptr) {
+				bool result = input->event_handler(input->id, &clickEvent);
+				if (! result) {
+					break;
+				}
+			}
+			input->focused = true;
+			input_render(input);
+			break;
+		}
+		case UI_EVENT_KEY: {
+			if (event->payload.keyboard.keyCode == KEY_ARROW_LEFT) {
+				if (input->cursor_x > 0) {
+					input->cursor_x -= 1;
+				} else {
+					if (input->cursor_x0 > 0) {
+						input->cursor_x0 -= 1;
+					}
+				}
+			}
+			if (event->payload.keyboard.keyCode <= 255) {
+				uint16_t input_width = input->bounding_rect.width;
+				if (input->bounding_rect.height == 3) {
+					input_width -= 2;
+				}
+				uint16_t x = input->cursor_x0 + input->cursor_x;
+				if (x < input->maxlen) {
+					input->value[x] = event->payload.keyboard.keyCode & 0xff;
+					if (input->cursor_x < input_width - 1) {
+						input->cursor_x++;
+					} else {
+						input->cursor_x0++;
+					}
+				}
+			}
+			input_render(input);
+			break;
 		}
 	}
 }
 
-bool rect_test_point_in_bounds(rect_t * rect, uint8_t x, uint8_t y)
+void component_set_focus(uint16_t componentCount, ui_component_t *components, uint16_t id)
 {
-	uint8_t x0 = rect->x;
-	uint8_t x1 = x0 + rect->width - 1;
-	uint8_t y0 = rect->y;
-	uint8_t y1 = y0 + rect->height - 1;
-	return (
-		x >= x0 && x <= x1 && y >= y0 && y <= y1
-	);
-}
-
-bool rect_test_mouse(rect_t * rect, uint16_t mouseX, uint16_t mouseY)
-{
-	return rect_test_point_in_bounds(rect, (uint8_t)mouseX/8, (uint8_t)mouseY/8);
-}
-
-void button_process_events(ui_button_t *button, ui_event_t *event)
-{
-	switch (event->type) {
-		case UI_EVENT_MOUSEMOVE:
-			if ((button->active)||
-					(rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)))
-			{
-				button_render(button);
-			}
-			break;
-		case UI_EVENT_MOUSEDOWN:
-			if (rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)) {
-				button->active = true;
-				button_render(button);
-			}
-			break;
-		case UI_EVENT_MOUSEUP:
-			if (button->active) {
-				button->active = false;
-				button_render(button);
-				if (rect_test_mouse(&(button->bounding_rect), event->payload.mouse.x, event->payload.mouse.y)) {
-					// emitEvent('click', button);
-				}
-			}
-			break;
-		case UI_EVENT_KEY:
-			// todo: shortcuts?
-			break;
-		default:
-			break;
+	for (uint16_t i = 0; i < componentCount; i++) {
+		components[i].component.generic.focused = components[i].component.generic.id == id;
 	}
 }
 
-void input_process_events(ui_input_t *input, ui_event_t *event)
+void component_process_events(uint16_t componentCount, ui_component_t *components, ui_event_t *event)
 {
-	switch (event->type) {
-
-	}
-}
-
-void component_process_events(uint8_t componentCount, ui_component_t *components, ui_event_t *event)
-{
-	for (uint8_t i = 0; i < componentCount; i++)
+	for (uint16_t i = 0; i < componentCount; i++)
 	{
+		bool oldFocus = components[i].component.generic.focused;
 		switch (components[i].type) {
 			case COMPONENT_BUTTON: {
 				button_process_events(&(components[i].component.button), event);
@@ -183,6 +257,10 @@ void component_process_events(uint8_t componentCount, ui_component_t *components
 				input_process_events(&(components[i].component.input), event);
 				break;
 			}
+		}
+		if ((oldFocus == false) && (components[i].component.generic.focused))
+		{
+			component_set_focus(componentCount, components, components[i].component.generic.id);
 		}
 	}
 }
@@ -203,7 +281,7 @@ void component_render(ui_component_t *component)
 	}
 }
 
-ui_component_t component_create_button(uint8_t id, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color)
+ui_component_t component_create_button(uint16_t id, const char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color)
 {
 	ui_component_t component = {0};
 	ui_button_t button = {0};
@@ -220,12 +298,11 @@ ui_component_t component_create_button(uint8_t id, char *label, uint8_t x, uint8
 	return component;
 }
 
-ui_component_t component_create_input(uint8_t id, char *label, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color, char *value, size_t maxlen)
+ui_component_t component_create_input(uint16_t id, uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color, char *value, size_t maxlen)
 {
 	ui_component_t component = {0};
 	ui_input_t input = {0};
 	rect_t rect = {0};
-	input.label = label;
 	rect.x = x;
 	rect.y = y;
 	rect.width = width;
