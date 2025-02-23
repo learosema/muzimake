ad3317e94d8ba0674677a1d90c6124dead94d0c3
Lea Rosema <lea@lea.lgbt>
2025-02-23 16:51:47 +0100

    chore: more work

--- src/textui/ui_event.h
+++ src/textui/ui_event.h
@@ -4,23 +4,26 @@
 #include <stdint.h>
 #include "mouse.h"
 
-#define UI_EVENT_NONE				0
+#define UI_EVENT_NONE       0
 #define UI_EVENT_KEYDOWN    1
-#define UI_EVENT_KEYUP			2
-#define UI_EVENT_KEY				4
+#define UI_EVENT_KEYUP      2
+#define UI_EVENT_KEY        4
 #define UI_EVENT_MOUSEDOWN  8
 #define UI_EVENT_MOUSEUP    16
-#define UI_EVENT_CLICK			32
-#define UI_EVENT_MOUSEMOVE	64
+#define UI_EVENT_CLICK      32
+#define UI_EVENT_MOUSEMOVE  64
 
-#define KEY_ARROW_LEFT      0x4b00
-#define KEY_ARROW_RIGHT     0x4d00
+#define KEY_ARROW_LEFT      0x4B00
+#define KEY_ARROW_RIGHT     0x4D00
 #define KEY_ARROW_UP        0x4800
 #define KEY_ARROW_DOWN      0x5000
 #define KEY_BACKSPACE       0x08
 #define KEY_ENTER           0x0D
 #define KEY_TAB             0x09
+#define KEY_SHIFT_TAB				0x0F00
 #define KEY_ESCAPE          0x1D
+#define KEY_ALT_X           0x2D00
+#define KEY_ALT_I           0x1700
 
 typedef struct ui_mouse_event_s
 {
