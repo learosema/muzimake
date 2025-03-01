4719e1d1c24c8b38d625a6320722b6f78fdda2f8
Lea Rosema <lea@lea.lgbt>
2025-03-01 22:21:11 +0100

    chore: more work

--- src/textui/component.cpp
+++ src/textui/component.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include "component.h"
@@ -20,6 +21,45 @@ bool rect_test_point_in_bounds(rect_t * rect, uint8_t x, uint8_t y)
 bool rect_test_mouse(rect_t * rect, uint16_t mouseX, uint16_t mouseY)
 {
 	return rect_test_point_in_bounds(rect, (uint8_t)(mouseX>>3), (uint8_t)(mouseY>>3));
+}
+
+rect_t get_clientrect(rect_t *bounding_rect)
+{
+	rect_t clientrect = *bounding_rect;
+	if (clientrect.height > 2 && clientrect.width > 2)
+	{
+		clientrect.x += 1;
+		clientrect.y += 1;
+		clientrect.width -= 2;
+		clientrect.height -= 2;
+	}
+	return clientrect;
+}
+
+void ui_handle_mouseup(ui_generic_t *generic, ui_event_t *event) {
+	if (event->type != UI_EVENT_MOUSEUP) {
+		return;
+	}
+	ui_event_t clickEvent = {0};
+	clickEvent.type = UI_EVENT_CLICK;
+	clickEvent.payload.click.target = generic->id;
+	clickEvent.payload.click.buttons = event->payload.mouse.buttons;
+	if (! rect_test_mouse(&(generic->bounding_rect), event->payload.mouse.x, event->payload.mouse.y))
+	{
+		if (generic->focused) {
+			generic->focused = false;
+			generic->paint = true;
+		}
+		return;
+	}
+	if (generic->event_handler != nullptr) {
+		bool result = generic->event_handler(generic->id, &clickEvent);
+		if (! result) {
+			return;
+		}
+	}
+	generic->focused = true;
+	generic->paint = true;
 }
 
 void button_render(ui_button_t *button)
@@ -184,26 +224,7 @@ void input_process_events(ui_input_t *input, ui_event_t *event)
 {
 	switch (event->type) {
 		case UI_EVENT_MOUSEUP: {
-			ui_event_t clickEvent = {0};
-			clickEvent.type = UI_EVENT_CLICK;
-			clickEvent.payload.click.target = input->id;
-			clickEvent.payload.click.buttons = event->payload.mouse.buttons;
-			if (! rect_test_mouse(&(input->bounding_rect), event->payload.mouse.x, event->payload.mouse.y))
-			{
-				if (input->focused) {
-					input->focused = false;
-					input->paint = true;
-				}
-				return;
-			}
-			if (input->event_handler != nullptr) {
-				bool result = input->event_handler(input->id, &clickEvent);
-				if (! result) {
-					break;
-				}
-			}
-			input->focused = true;
-			input->paint = true;
+			ui_handle_mouseup((ui_generic_t *)input, event);
 			break;
 		}
 		case UI_EVENT_KEY: {
@@ -295,13 +316,15 @@ void input_process_events(ui_input_t *input, ui_event_t *event)
 
 void listbox_render(ui_listbox_t *listbox)
 {
+	uint8_t lbcolor = listbox->color;
 	if (listbox->focused) {
+		lbcolor = lbcolor & 15;
 		textmode_dblrect(
 			listbox->bounding_rect.x,
 			listbox->bounding_rect.y,
 			listbox->bounding_rect.width,
 			listbox->bounding_rect.height,
-			listbox->color
+			lbcolor
 		);
 	} else {
 		textmode_rect(
@@ -309,17 +332,17 @@ void listbox_render(ui_listbox_t *listbox)
 			listbox->bounding_rect.y,
 			listbox->bounding_rect.width,
 			listbox->bounding_rect.height,
-			listbox->color
+			lbcolor
 		);
 	}
 	uint8_t inner_width = listbox->bounding_rect.width - 2;
 	uint8_t inner_height = listbox->bounding_rect.height - 2;
-	uint8_t select_color = 0xf + (listbox->color & 0x70 ^ 0x70);
+	uint8_t select_color = 0x70;
 	for (uint8_t y = 0; y < inner_height; y++)
 	{
 		uint16_t yy = listbox->cursor_y0 + y;
 		uint8_t color = yy == (listbox->cursor_y0 + listbox->cursor_y) ?
-			select_color : listbox->color;
+			select_color : lbcolor;
 		if (yy < listbox->num_items) {
 			uint8_t written = textmode_printn(listbox->values[yy], inner_width,
 				listbox->bounding_rect.x + 1,
@@ -344,26 +367,7 @@ void listbox_process_events(ui_listbox_t *listbox, ui_event_t *event)
 {
 	switch (event->type) {
 		case UI_EVENT_MOUSEUP: {
-			ui_event_t clickEvent = {0};
-			clickEvent.type = UI_EVENT_CLICK;
-			clickEvent.payload.click.target = listbox->id;
-			clickEvent.payload.click.buttons = event->payload.mouse.buttons;
-			if (! rect_test_mouse(&(listbox->bounding_rect), event->payload.mouse.x, event->payload.mouse.y))
-			{
-				if (listbox->focused) {
-					listbox->focused = false;
-					listbox->paint = true;
-				}
-				return;
-			}
-			if (listbox->event_handler != nullptr) {
-				bool result = listbox->event_handler(listbox->id, &clickEvent);
-				if (! result) {
-					break;
-				}
-			}
-			listbox->focused = true;
-			listbox->paint = true;
+			ui_handle_mouseup((ui_generic_t *)listbox, event);
 			break;
 		}
 		case UI_EVENT_KEY: {
@@ -403,34 +407,107 @@ void range_render(ui_range_t *range)
 	if (range->active || range->focused) {
 		color = color & 0x0f;
 	}
-	if (range->bounding_rect.height != 1) {
-		range->bounding_rect.height = 1;
+	if (range->bounding_rect.height != 1 && range->bounding_rect.height != 3) {
+		range->bounding_rect.height = 3;
+	}
+	if (range->bounding_rect.width < 10) {
+		range->bounding_rect.width = 10;
 	}
 
-	uint8_t inner_width = (range->bounding_rect.width - 2);
-	float f = (float)(inner_width) / (float)(range->max - range->min + 1);
+	rect_t clientrect = get_clientrect(&(range->bounding_rect));
+	clientrect.width -= 4;
+  if (range->bounding_rect.height == 3)
+	{
 
-	uint8_t x0 = range->bounding_rect.x;
-	uint8_t x1 = x0 + range->bounding_rect.width - 1;
-	uint8_t xm = x0 + 1  + CLAMP((int)((range->value - range->min) * f + .5), 0, inner_width);
-	uint8_t y  = range->bounding_rect.y;
+		if (range->focused) {
+			textmode_dblrect(
+				range->bounding_rect.x,
+				range->bounding_rect.y,
+				range->bounding_rect.width,
+				range->bounding_rect.height,
+				color
+			);
+		} else {
+			textmode_rect(
+				range->bounding_rect.x,
+				range->bounding_rect.y,
+				range->bounding_rect.width,
+				range->bounding_rect.height,
+				color
+			);
+		}
+	}
+
+	uint8_t slider_width = (clientrect.width - 2);
+	float f = (float)(slider_width) / (float)(range->max - range->min + 1);
+
+	uint8_t x0 = clientrect.x;
+	uint8_t x1 = x0 + clientrect.width - 1;
+	uint8_t xm = x0 + 1  + CLAMP((int)((float)(range->value - range->min) * f + .5f), 0, slider_width);
+	uint8_t y  = clientrect.y;
 
 	char stringValue[20];
-	// TODO: pad string....
-	sprintf(stringValue,"%d  ", inner_width, range->value, f);
-	textmode_print(stringValue, x1 + 2, y, color);
+	sprintf(stringValue,"%4d", range->value);
+
+	textmode_print(stringValue, x1 + 1, y, color);
 	textmode_putchar_color(x0, y, CP_LEFT_TRIANGLE, color);
 	textmode_hline(x0 + 1, y, xm - x0 - 1, CP_MEDIUM_SHADE, color);
-	textmode_hline(xm + 1, y, inner_width - xm + x0, CP_MEDIUM_SHADE, color);
+	textmode_hline(xm + 1, y, slider_width - xm + x0, CP_MEDIUM_SHADE, color);
 	textmode_putchar_color(xm, y, CP_SQUARE, color);
 	textmode_putchar_color(x1, y, CP_RIGHT_TRIANGLE, color);
 }
 
 void range_process_events(ui_range_t *range, ui_event_t *event)
 {
-	// TODO
+	switch (event->type) {
+		case UI_EVENT_MOUSEMOVE:
+		case UI_EVENT_MOUSEDOWN: {
+			if (event->payload.mouse.buttons & 1 > 0) {
+				rect_t clientrect = get_clientrect(&(range->bounding_rect));
+				clientrect.width -= 4;
+				if ((clientrect.width<3) || (clientrect.height < 1)) {
+					char debug[100];
+					sprintf(debug, "MUH %d %d %d %d", clientrect.x, clientrect.y, clientrect.width, clientrect.height);
+					textmode_print(debug, 33, 4, 0x5f);
+					return;
+				}
+				if (rect_test_mouse(&clientrect, event->payload.mouse.x, event->payload.mouse.y)) {
+					uint8_t x = (uint8_t)(event->payload.mouse.x / 8);
+					x = x - clientrect.x - 1;
+					if (x >= 0 && x < clientrect.width - 1) {
+						float inner_width = (float)(clientrect.width - 2);
+						float f = (float)(x) / inner_width;
+						int newValue = range->min + (int)(f * (float)(range->max - range->min + 1) + 0.5f);
+						range->value = CLAMP(newValue, range->min, range->max);
+						range->paint = true;
+					}
+				}
+			}
+			break;
+		}
+		case UI_EVENT_MOUSEUP: {
+			ui_handle_mouseup((ui_generic_t *)range, event);
+			break;
+		}
+	}
+	if (event->type == UI_EVENT_KEY) {
+		if (range->focused == false) {
+			return;
+		}
+		switch (event->payload.keyboard.keyCode) {
+			case KEY_ARROW_LEFT: {
+				range->value = MAX(range->min, range->value - range->step);
+				range->paint = true;
+				break;
+			}
+			case KEY_ARROW_RIGHT: {
+				range->value = MIN(range->max, range->value + range->step);
+				range->paint = true;
+				break;
+			}
+		}
+	}
 }
-
 
 void component_set_focus(uint16_t count, ui_component_t *components, uint16_t id)
 {
@@ -501,6 +578,9 @@ void component_process_events(uint16_t count, ui_component_t *components, ui_eve
 			}
 			case COMPONENT_LISTBOX: {
 				listbox_process_events(&(components[i].component.listbox), event);
+				break;
+			case COMPONENT_RANGE:
+				range_process_events(&(components[i].component.range), event);
 				break;
 			}
 		}
