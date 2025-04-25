9038f15ed9b3b3085853f9bf8c29bade5f7d9ebf
Lea Rosema <lea@lea.lgbt>
2025-04-25 22:22:53 +0200

    chore: more work

--- tests/test_tmr.cpp
+++ tests/test_tmr.cpp
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #ifdef __DOS__
-#include <conio.h>
 #include <dos.h>
 #else
 #include <stubs.h>
 #endif
 
 #include "timer.h"
+#include <dpmiutil.h>
+static volatile int counter = 0;
+// https://open-watcom.github.io/open-watcom-v2-wikidocs/clib.html#_dos_getvect
+
+#ifdef __DOS__
+void (__interrupt __far *prev_timer_func)();
+void __interrupt __far timer_func(void)
+{
+	counter++;
+
+	_chain_intr(prev_timer_func);
+}
+#endif
 
 int main(int argc, char const *argv[])
 {
 	int ch = 0;
-	timer_init();
-	atexit(timer_shutdown);
-	while (ch != 27) {
-		printf("Main program");
+
+	#ifdef __DOS__
+	// dpmi_lock_linear_region((uint32_t)(&counter), 4);
+	prev_timer_func = _dos_getvect(0x8);
+	_dos_setvect(0x8, timer_func);
+	#endif
+
+	while (1) {
+		printf("Main program %d\n", counter);
 		delay(1000);
-		if (kbhit()) {
-			ch = getch();
-		}
 	}
+	#ifdef __DOS__
+	_dos_setvect(0x8, prev_timer_func);
+	#endif
 	return 0;
 }
