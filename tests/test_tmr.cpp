3c3f9111e73dc89e42bf440109ab3cc6e6e60980
Lea Rosema <lea@lea.lgbt>
2025-04-27 16:01:04 +0200

    chore: more work on timer

--- tests/test_tmr.cpp
+++ tests/test_tmr.cpp
@@ -11,24 +11,17 @@
 #include "timer.h"
 #include <dpmiutil.h>
 static volatile int counter = 0;
-// https://open-watcom.github.io/open-watcom-v2-wikidocs/clib.html#_dos_getvect
-
-#ifdef __DOS__
 
 
 
-void (__interrupt __far *prev_timer_func)();
-void __interrupt __far timer_func(void)
+static void timer_func(uint64_t time_value)
 {
 	counter++;
+
 	// we can print via textmode_print
 	textmode_print("Muh", 0, 0, (counter & 0x0f) + 16);
-	// can we malloc here?
-	void *test= malloc(16);
-	free(test);
-	_chain_intr(prev_timer_func);
 }
-#endif
+
 
 int main(int argc, char const *argv[])
 {
@@ -36,15 +29,8 @@ int main(int argc, char const *argv[])
 	int ch = 0;
 	textmode_setmode(3);
 	textmode_clear(0x1f);
-	textmode_print("Hit ESC to quit", 0, 49, 0x1a);
-	uint16_t orig_count = timer_get_pit_count();
-	timer_set_pit_count(timer_calc_freq(1000));
-
-	#ifdef __DOS__
-	// dpmi_lock_linear_region((uint32_t)(&counter), 4);
-	prev_timer_func = _dos_getvect(0x8);
-	_dos_setvect(0x8, timer_func);
-	#endif
+	textmode_print("Hit ESC to quit", 0, 24, 0x1a);
+	timer_init(timer_func);
 
 	while (ch != 27) {
 		if (kbhit()) {
@@ -52,13 +38,9 @@ int main(int argc, char const *argv[])
 		}
 		snprintf(buf, 256, "Main program: %d", counter);
 		textmode_print(buf, 0, 2, 0x1e);
-		delay(1000);
+		timer_delay(1000);
 	}
 	textmode_setmode(3);
-
-	timer_set_pit_count(orig_count);
-	#ifdef __DOS__
-	_dos_setvect(0x8, prev_timer_func);
-	#endif
+	timer_shutdown();
 	return 0;
 }
