196b7081c5dad2d0d9d6160ec76b44162524f43a
Lea Rosema <lea@lea.lgbt>
2025-08-26 01:08:11 +0200

    chore: test stuff

--- tests/test_cmp.cpp
+++ tests/test_cmp.cpp
@@ -5,8 +5,26 @@
 #include "textui/cmponent.h"
 #include "vendor/cp437.h"
 
+static ui_component_t button;
+const char *label = "OK";
+const uint8_t button_id = 1;
+const uint8_t button_x = 0;
+const uint8_t button_y = 0;
+const uint8_t button_width = 10;
+const uint8_t button_height = 3;
+const uint8_t button_color = 0x2f;
+
+
 static void before_each(void *data) {
   textmode_init_headless(3);
+
+	button = component_create_button(
+		button_id,
+		label,
+		button_x, button_y,
+		button_width, button_height,
+		button_color
+	);
 }
 
 static void after_each(void *data) {
@@ -16,50 +34,89 @@ static void after_each(void *data) {
 #define GET_CHAR(info, x, y) (uint8_t)(*(info->vram + 2 * (info->numCols * (y) + (x))))
 #define GET_COLOR(info, x, y) (uint8_t)(*(info->vram + 1 + 2 * (info->numCols * (y) + (x))))
 
+bool check_thin_box(uint8_t x, uint8_t y, uint8_t width, uint8_t height)
+{
+	MODEINFO* info = textmode_get_modeinfo();
+	char topleft_corner = GET_CHAR(info, x, y);
+	char topright_corner = GET_CHAR(info, x + width - 1, 0);
+	char btmleft_corner = GET_CHAR(info, y, y + height - 1);
+	char btmright_corner = GET_CHAR(info, x + width - 1, y + height - 1);
+	if (CP_THIN_RIGHT_THIN_DOWN != topleft_corner) return false;
+	if (CP_THIN_LEFT_THIN_DOWN != topright_corner) return false;
+	if (CP_THIN_RIGHT_THIN_UP != btmleft_corner) return false;
+	if (CP_THIN_LEFT_THIN_UP != btmright_corner) return false;
+	for (uint8_t i = 1; i < width - 2; i++) {
+		if (CP_THIN_HORIZONTAL != GET_CHAR(info, x + i, y)) return false;
+		if (CP_THIN_HORIZONTAL != GET_CHAR(info, x + i, y + height - 1)) return false;
+	}
+	for (uint8_t i = 1; i < height - 2; i++) {
+		if (CP_THIN_HORIZONTAL != GET_CHAR(info, x, y + i)) return false;
+		if (CP_THIN_HORIZONTAL != GET_CHAR(info, x + width - 1, y + i)) return false;
+	}
+	return true;
+}
+
+bool check_thick_box(uint8_t x, uint8_t y, uint8_t width, uint8_t height)
+{
+	MODEINFO* info = textmode_get_modeinfo();
+	char topleft_corner = GET_CHAR(info, x, y);
+	char topright_corner = GET_CHAR(info, x + width - 1, 0);
+	char btmleft_corner = GET_CHAR(info, y, y + height - 1);
+	char btmright_corner = GET_CHAR(info, x + width - 1, y + height - 1);
+	if (CP_THICK_RIGHT_THICK_DOWN != topleft_corner) return false;
+	if (CP_THICK_LEFT_THICK_DOWN != topright_corner) return false;
+	if (CP_THICK_RIGHT_THICK_UP != btmleft_corner) return false;
+	if (CP_THICK_LEFT_THICK_UP != btmright_corner) return false;
+	for (uint8_t i = 1; i < width - 2; i++) {
+		if (CP_THICK_HORIZONTAL != GET_CHAR(info, x + i, y)) return false;
+		if (CP_THICK_HORIZONTAL != GET_CHAR(info, x + i, y + height - 1)) return false;
+	}
+	for (uint8_t i = 1; i < height - 2; i++) {
+		if (CP_THICK_HORIZONTAL != GET_CHAR(info, x, y + i)) return false;
+		if (CP_THICK_HORIZONTAL != GET_CHAR(info, x + width - 1, y + i)) return false;
+	}
+	return true;
+}
+
+
 TEST button_should_render()
 {
-	const char *label = "OK";
-	const uint8_t button_id = 1;
-	const uint8_t button_x = 0;
-	const uint8_t button_y = 0;
-	const uint8_t button_width = 10;
-	const uint8_t button_height = 3;
-	const uint8_t button_color = 0x2f;
 
-	ui_component_t button = component_create_button(
-		button_id,
-		label,
-		button_x, button_y,
-		button_width, button_height,
-		button_color
-	);
 	component_render(&button);
-
-
 	MODEINFO* info = textmode_get_modeinfo();
-
-	uint8_t color = GET_COLOR(info, 0, 0);
-	char topleft_corner = GET_CHAR(info, 0, 0);
-	char topright_corner = GET_CHAR(info, button_width - 1, 0);
-	char btmleft_corner = GET_CHAR(info, 0, button_height - 1);
-	char btmright_corner = GET_CHAR(info, button_width - 1, button_height - 1);
+	uint8_t color = GET_COLOR(info, button_x, button_y);
 
 	ASSERT_EQ(button_color, color);
-	ASSERT_EQ(CP_THIN_RIGHT_THIN_DOWN, topleft_corner);
-	ASSERT_EQ(CP_THIN_LEFT_THIN_DOWN, topright_corner);
-	ASSERT_EQ(CP_THIN_RIGHT_THIN_UP, btmleft_corner);
-	ASSERT_EQ(CP_THIN_LEFT_THIN_UP, btmright_corner);
-	for (uint8_t x = 1; x < button_width - 2; x++) {
-		ASSERT_EQ(CP_THIN_HORIZONTAL, GET_CHAR(info, x, 0));
-		ASSERT_EQ(CP_THIN_HORIZONTAL, GET_CHAR(info, x, button_height - 1));
-	}
-	for (uint8_t y = 1; y < button_height - 2; y++) {
-		ASSERT_EQ(CP_THIN_HORIZONTAL, GET_CHAR(info, 0, y));
-		ASSERT_EQ(CP_THIN_HORIZONTAL, GET_CHAR(info, button_width - 1, y));
-	}
+	ASSERT_EQm("Screen should have a box at the given coords", true, check_thin_box(button_x, button_y, button_width, button_height));
 
 	PASS();
 }
+
+
+TEST button_should_render_thick_when_focused()
+{
+	button.component.button.focused = true;
+	component_render(&button);
+	MODEINFO* info = textmode_get_modeinfo();
+	uint8_t color = GET_COLOR(info, button_x, button_y);
+
+	ASSERT_EQ(button_color, color);
+	ASSERT_EQm("Screen should have a box at the given coords", true, check_thick_box(button_x, button_y, button_width, button_height));
+
+	PASS();
+}
+
+TEST button_should_render_black_when_active()
+{
+	button.component.button.active = true;
+	component_render(&button);
+	MODEINFO* info = textmode_get_modeinfo();
+	uint8_t color = GET_COLOR(info, button_x, button_y);
+
+	ASSERT_EQ(button_color & 0xf, color);
+	PASS();
+}
+
 
 SUITE(button_tests)
 {
@@ -67,6 +124,8 @@ SUITE(button_tests)
 	SET_TEARDOWN(after_each, nullptr);
 
 	RUN_TEST(button_should_render);
+	RUN_TEST(button_should_render_thick_when_focused);
+	RUN_TEST(button_should_render_black_when_active);
 }
 
 
