02b3bbe05984f40ca68467b28fe314e94109b45b
Lea Rosema <lea@lea.lgbt>
2025-09-30 16:15:04 +0200

    chore: intermediary work

--- src/helper/list.cpp
+++ src/helper/list.cpp
@@ -32,46 +32,61 @@ void linked_list_dispose(linked_list_t *list)
 	free(list);
 }
 
-void linked_list_append(linked_list_t *list, node_t *node)
+void linked_list_append_node(linked_list_t *list, node_t *node)
 {
-	if (list->tail == nullptr) {
+	if (list->tail == NULL) {
 		list->head = node;
 		list->tail = node;
-		node->next = nullptr;
+		node->next = NULL;
 		return;
 	}
 
 	node_t *old_tail = list->tail;
 	list->tail->next = node;
 	list->tail = node;
-	node->next = nullptr;
+	node->next = NULL;
 }
 
-void linked_list_insert_after(linked_list_t *list, node_t *new_node, node_t *ref_node)
+void linked_list_insert_node_after(linked_list_t *list, node_t *new_node, node_t *ref_node)
 {
 	for (node_t *iter = list->head; iter != NULL; iter = iter->next)
 	{
-		if (iter == ref_node)
+		if (iter != ref_node)
 		{
-			new_node->next = iter->next;
-			iter->next = new_node;
-			return;
+			continue;
 		}
+
+		if (iter == list->tail) {
+			list->tail = new_node;
+		}
+
+		new_node->next = iter->next;
+		iter->next = new_node;
+		return;
 	}
 	list->tail->next = new_node;
 	list->tail = new_node;
 	new_node->next = NULL;
 }
 
-bool linked_list_remove(linked_list_t *list, node_t *node)
+bool linked_list_remove_node(linked_list_t *list, node_t *node)
 {
+	if (node == list->head) {
+		list->head = list->head->next;
+		return true;
+	}
 	for (node_t *iter = list->head; iter != NULL; iter = iter->next)
 	{
-		if (iter->next == node) {
-			iter->next = node->next;
-			linked_list_dispose_node(node);
-			return true;
+		if (iter->next != node) {
+			continue;
 		}
+		iter->next = node->next;
+		if (node == list->tail) {
+			list->tail = iter;
+			iter->next = NULL;
+		}
+		linked_list_dispose_node(node);
+		return true;
 	}
 	return false;
 }
@@ -98,4 +113,61 @@ node_t *linked_list_node_at(linked_list_t * list, int node_index)
 		current_index++;
 	}
 	return NULL;
+}
+
+node_t *linked_list_prev_node(linked_list_t *list, node_t *ref_node)
+{
+	if (ref_node == list->head)
+	{
+		return NULL;
+	}
+	for (node_t *iter = list->head; iter != NULL; iter = iter->next)
+	{
+		if (iter->next == ref_node) {
+			return iter;
+		}
+	}
+	return NULL;
+}
+
+void linked_list_unshift_node(linked_list_t *list, node_t *new_node)
+{
+	if (list->tail == NULL)
+	{
+		list->head = new_node;
+		list->tail = new_node;
+		new_node->next = NULL;
+		return;
+	}
+
+	node_t *old_head = list->head;
+	list->head = new_node;
+	list->head->next = old_head;
+}
+
+void linked_list_insert_node_before(linked_list_t *list, node_t *new_node, node_t *ref_node)
+{
+	if (ref_node == list->head) {
+		linked_list_unshift_node(list, new_node);
+		return;
+	}
+	node_t *prev = linked_list_prev_node(list, ref_node);
+	linked_list_insert_node_after(list, new_node, prev);
+}
+
+void linked_list_append(linked_list_t *list, void *data)
+{
+	linked_list_append_node(list, linked_list_create_node(data));
+}
+
+void linked_list_insert_after(linked_list_t *list, void *data, int node_index)
+{
+	node_t *ref_node = linked_list_node_at(list, node_index);
+	node_t *new_node = linked_list_create_node(data);
+	if ((ref_node == NULL) || (node_index < 0))
+	{
+		linked_list_append_node(list, new_node);
+		return;
+	}
+	linked_list_insert_node_after(list, new_node, ref_node);
 }
--- tests/test_lst.cpp
+++ tests/test_lst.cpp
@@ -29,7 +29,7 @@ TEST list_append_to_new_list_should_work()
 	linked_list_t * list = linked_list_new();
 	int dummy_data = 1337;
 	node_t *dummy_node = linked_list_create_node(&dummy_data);
-	linked_list_append(list, dummy_node);
+	linked_list_append_node(list, dummy_node);
 
 	ASSERT_EQ(&dummy_data, list->head->data);
 	ASSERT_EQ(dummy_node, list->head);
@@ -50,13 +50,13 @@ TEST list_append_second_element_should_work()
 	node_t *node_one = linked_list_create_node(&one);
 	node_t *node_two = linked_list_create_node(&two);
 
-	linked_list_append(list, node_one);
-	linked_list_append(list, node_two);
+	linked_list_append_node(list, node_one);
+	linked_list_append_node(list, node_two);
 
 	ASSERT_EQm("Node 1 should be at the list head", node_one, list->head);
 	ASSERT_EQm("Node 2 should be at the list tail", node_two, list->tail);
-	ASSERT_EQm("list.head->next should point to node 2", node_two, list->head->next);
-	ASSERT_EQm("list.tail->next shuold be NULL", NULL, list->tail->next);
+	ASSERT_EQm("list->head->next should point to node 2", node_two, list->head->next);
+	ASSERT_EQm("list->tail->next shuold be NULL", NULL, list->tail->next);
 
 	linked_list_dispose(list);
 
@@ -74,9 +74,9 @@ TEST list_append_third_element_should_work()
 	node_t *node_two = linked_list_create_node(&two);
 	node_t *node_three = linked_list_create_node(&three);
 
-	linked_list_append(list, node_one);
-	linked_list_append(list, node_two);
-	linked_list_append(list, node_three);
+	linked_list_append_node(list, node_one);
+	linked_list_append_node(list, node_two);
+	linked_list_append_node(list, node_three);
 
 	ASSERT_EQm("Node 1 should be at the list head", node_one, list->head);
 	ASSERT_EQm("Node 3 should be at the list tail", node_three, list->tail);
@@ -88,7 +88,103 @@ TEST list_append_third_element_should_work()
 	PASS();
 }
 
+TEST list_remove_node_should_work()
+{
+	linked_list_t *list = linked_list_new();
+	int one = 1;
+	int two = 2;
+	int three = 3;
 
+	node_t *node_one = linked_list_create_node(&one);
+	node_t *node_two = linked_list_create_node(&two);
+	node_t *node_three = linked_list_create_node(&three);
+
+	linked_list_append_node(list, node_one);
+	linked_list_append_node(list, node_two);
+	linked_list_append_node(list, node_three);
+
+	linked_list_remove_node(list, node_two);
+
+	ASSERT_EQm("Node 1 should be at the list head", node_one, list->head);
+	ASSERT_EQm("Node 3 should be at the list tail", node_three, list->tail);
+	ASSERT_EQm("Node 1->next should point to node 3", node_three, node_one->next);
+	ASSERT_EQm("list->head->next should point to node 3", node_three, list->head->next);
+
+	linked_list_dispose(list);
+	PASS();
+}
+
+
+TEST list_remove_node_at_head_should_work()
+{
+	linked_list_t *list = linked_list_new();
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	node_t *node_one = linked_list_create_node(&one);
+	node_t *node_two = linked_list_create_node(&two);
+	node_t *node_three = linked_list_create_node(&three);
+
+	linked_list_append_node(list, node_one);
+	linked_list_append_node(list, node_two);
+	linked_list_append_node(list, node_three);
+
+	linked_list_remove_node(list, node_one);
+
+	ASSERT_EQm("Node 2 should be at the list head", node_two, list->head);
+	ASSERT_EQm("Node 3 should be at the list tail", node_three, list->tail);
+	ASSERT_EQm("list->head->next should point to node 3", node_three, list->head->next);
+
+	linked_list_dispose(list);
+	PASS();
+}
+
+TEST list_remove_node_at_tail_should_work()
+{
+	linked_list_t *list = linked_list_new();
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	node_t *node_one = linked_list_create_node(&one);
+	node_t *node_two = linked_list_create_node(&two);
+	node_t *node_three = linked_list_create_node(&three);
+
+	linked_list_append_node(list, node_one);
+	linked_list_append_node(list, node_two);
+	linked_list_append_node(list, node_three);
+
+	linked_list_remove_node(list, node_three);
+
+	ASSERT_EQm("Node 1 should be at the list head", node_one, list->head);
+	ASSERT_EQm("Node 2 should be at the list tail", node_two, list->tail);
+	ASSERT_EQm("list->tail->next should point NULL", NULL, list->tail->next);
+
+	linked_list_dispose(list);
+	PASS();
+}
+
+TEST list_insert_node_after_refnode_should_work()
+{
+	linked_list_t *list = linked_list_new();
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	node_t *node_one = linked_list_create_node(&one);
+	node_t *node_two = linked_list_create_node(&two);
+
+	linked_list_append_node(list, node_one);
+	linked_list_insert_node_after(list, node_two, node_one);
+
+	ASSERT_EQm("Node 1 should be at the list head", node_one, list->head);
+	ASSERT_EQm("list->head->next should point to node 2", node_two, list->head->next);
+	ASSERT_EQm("list->tail->next should be NULL", NULL, list->tail->next);
+
+	linked_list_dispose(list);
+	PASS();
+}
 
 SUITE(linked_list_tests)
 {
@@ -98,6 +194,9 @@ SUITE(linked_list_tests)
 	RUN_TEST(list_should_initialize);
 	RUN_TEST(list_append_to_new_list_should_work);
 	RUN_TEST(list_append_third_element_should_work);
+	RUN_TEST(list_remove_node_should_work);
+	RUN_TEST(list_remove_node_at_head_should_work);
+	RUN_TEST(list_remove_node_at_tail_should_work);
 }
 
 GREATEST_MAIN_DEFS();
